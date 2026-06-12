af634c2c20edd18a1a22d2eeab50d52d3847eeea
MiguelFaraj-Eng <miguel.faraj@technicaintl.com>
2026-06-12 17:25:12 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@
 
 const CONFIG = {
   // --- GitHub repo that acts as the database + file store ---
-  GITHUB_TOKEN: "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",
+  GITHUB_TOKEN: "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",
   GITHUB_OWNER: "MiguelFaraj-Eng",
   GITHUB_REPO: "Electrical-Standardization",   // recommend a SEPARATE private repo for data
   GITHUB_BRANCH: "main",
